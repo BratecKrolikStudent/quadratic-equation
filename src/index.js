34cbe4fb982a5f8f1145eaf1c8c5e5ab57919b5f
Dmitriy Kunets <t38@tut.by>
2018-09-11 01:12:21 +0300

    task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  let arr = equation.match(/[^\^][\-? ?]?\d+/g).map(item => +item.replace(/\s+/g,'').toString());
+  let [ a, b, c ] = arr;
+
+  b = b / a;
+  c = c / a;
+  a = a / a;
+
+  let d = Math.pow(b, 2) - 4 * a * c;
+  let x1 = (-b + Math.sqrt(d)) / 2*a;
+  let x2 = (-b - Math.sqrt(d)) / 2*a;
+
+  return [ x2, x1 ];
 }
